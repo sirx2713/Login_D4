07390ca7faae461b3737e33ea70f2df5a29300c2
sirx2713 <bradx2713@gmail.com>
2024-03-15 00:35:36 +0300

    Initial Commit

---
+++ Login_D4/Login_D4App.swift
@@ -0,0 +1,17 @@
+//
+//  Login_D4App.swift
+//  Login_D4
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/15.
+//
+
+import SwiftUI
+
+@main
+struct Login_D4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
