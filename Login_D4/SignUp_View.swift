805b5675bd7ea8c92050416ce41351bb475ae435
sirx2713 <bradx2713@gmail.com>
2024-03-15 02:18:25 +0300

    04 Attempts making a Login UI.

---
+++ Login_D4/SignUp_View.swift
@@ -0,0 +1,18 @@
+//
+//  SignUp_View.swift
+//  Login_D4
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/15.
+//
+
+import SwiftUI
+
+struct SignUp_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUp_View()
+}
